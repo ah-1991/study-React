f2653fa563e1e7b4d680d71eb8a3721112377bcb
a.h <hoge@co.jp>
2018-11-25 23:43:03 +0900

    拡張03 ボードをループで作成

--- 01_tutorial-app/src/index.js
+++ 01_tutorial-app/src/index.js
@@ -13,29 +13,33 @@ function Square(props) {
 class Board extends React.Component {
   renderSquare(i) {
     return <Square
+      key={i}
       value={this.props.squares[i]}
       onClick={() => this.props.onClick(i)}
     />;
   }
 
+  renderBoardRow(array) {
+    return (
+      array.map((elm, index) => {
+        return this.renderSquare(elm);
+      })
+    );
+  }
+
   render() {
+    const boardRow = Array(3).fill(null).map((_, i) => i);
+    const boardRows = boardRow.map((row, index, array) => {
+      return (
+        <div className="board-row" key={index}>
+          {this.renderBoardRow(array.map(elm => elm + index * 3))}
+        </div>
+      );
+    });
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {boardRows}
       </div>
     );
   }
